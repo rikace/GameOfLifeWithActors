6cc8453f3b98d35f1a8cc01f50818bd0df1ea472
Riccardo <tericcardo@gmail.com>
2017-03-09 18:57:35 -0600

    first commit

---
+++ GameOfLife/GameOfLifeAkka/Program.fs
@@ -0,0 +1,23 @@
+﻿open GameOfLife
+open System
+open System.Windows
+open System.Threading
+open System.Windows.Threading
+open System.Windows.Media.Imaging
+open GameOfLifeAgent
+
+[<System.STAThread>]
+[<EntryPoint>]
+let main argv =
+
+    SynchronizationContext.SetSynchronizationContext(
+        new DispatcherSynchronizationContext(
+            Dispatcher.CurrentDispatcher))
+
+    let form = Window(Content=image, Title="Game of Life")
+    let application = new Application()
+    use dipose = GameOfLifeAgent.run()
+    application.Run(form) |> ignore
+
+    0
+
